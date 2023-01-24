29b89cf52636c5a9bce5a763bd51128490529f45
David DyTang <116765045+ddytang-fi@users.noreply.github.com>
2023-01-23 18:37:07 -0800

    Add new Hotelbeds Availability Parser

---
+++ pages/2_HB_Availability_Parser.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import json
+
+st.set_page_config(
+    page_title="Hotelbeds Availability Parser",
+    page_icon="💡"
+)
+st.sidebar.header("Hotelbeds Availability Parser")
+
+st.title("Hotelbeds Availability Parser")
+
+hba_uploaded_file = st.file_uploader("Select json file to upload", type=["json"])
+
+if jba_uploaded_file is not None:
+    export_filename = hba_uploaded_file.name.replace(".json","") + ".csv"
+
+    # Read and unnest JSON file
+    data = json.load(hba_uploaded_file)
+    df_availability = pd.json_normalize(data, record_path=["hotels", "hotels", "rooms", "rates"]
+                      , meta=[["hotels", "hotels", "currency"], ["hotels", "hotels", "code"], ["hotels", "hotels", "name"], ["hotels", "hotels", "rooms", "code"], ["hotels", "hotels", "rooms", "name"]]
+                      , errors="ignore")
+    df_export = df[["hotels.hotels.code", "hotels.hotels.name", "hotels.hotels.rooms.code", "hotels.hotels.rooms.name", "hotels.hotels.currency", "net", "packaging", "rateType", "rateKey"]]
+
+    with open("export_hba.csv","w") as f:
+        df_export.to_csv(f)
+    with open("export_hba.csv") as f:
+        st.download_button(
+            label="Download Parsed Availability File", 
+            data=f, 
+            file_name=export_filename,
+            mime="text/csv"
+        )
---
+++ pages/3_AI_Parser.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import re
+import json
+
+st.set_page_config(
+    page_title="Aggregate Intelligence Parser",
+    page_icon="💰"
+)
+st.sidebar.header("Aggregate Intelligence Parser")
+
+st.title("Aggregate Intelligence Parser")
+
+ai_uploaded_file = st.file_uploader("Select json file to upload", type=["json"])
+
+if ai_uploaded_file is not None:
+    export_filename = ai_uploaded_file.name.replace(".json","") + ".csv"
+
+    # Read JSON File
+    df = pd.read_json(ai_uploaded_file)
+    df["roomtype"] = np.where(df["ratedescription"].str.contains("1 sofa bed"), "Sofa Bed + ", "")
+    df["roomtype"] = df["roomtype"] + np.where(df["status_code"] == 203, "Hotel not listed",
+                            np.where(df["status_code"] == 202, "No rooms available",
+                            np.where((df["ratedescription"].str.contains("1 queen")) & (df["ratedescription"].str.contains("1 king")), "King + Queen",
+                            np.where(df["ratedescription"].str.contains("2 queen"), "Queen Queen",
+                            np.where(df["ratedescription"].str.contains("1 queen"), "Queen",
+                            np.where(df["ratedescription"].str.contains("2 king"), "King King",
+                            np.where(df["ratedescription"].str.contains("1 king"), "King",
+                            np.where(df["ratedescription"].str.contains("2 full"), "Double Double",
+                            np.where(df["ratedescription"].str.contains("1 full"), "Double",
+                            np.where(df["ratedescription"].str.contains("2 twin"), "Twin Twin",
+                            np.where(df["ratedescription"].str.contains("1 twin"), "Twin",
+                            np.where(df["ratedescription"].str.contains("4 bunk"), "Bunk Bunk Bunk Bunk",
+                            "Other"))))))))))))
+    df_export = df[["hotelcode", "ratedate", "roomtype", "onsiterate", "status_code", "ratedescription", "taxtype"]]
+
+    with open("export_ai.csv","w") as f:
+        df_export.to_csv(f)
+    with open("export_ai.csv") as f:
+        st.download_button(
+            label="Download Parsed Rack Rates File", 
+            data=f, 
+            file_name=export_filename,
+            mime="text/csv"
+        )
