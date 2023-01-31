bd7c5329ca5040ad6ac81ad6ad3d8eff32e8b710
David DyTang <116765045+ddytang-fi@users.noreply.github.com>
2023-01-31 12:08:15 -0800

    Add additional rate description parsing

--- pages/3_AI_Parser.py
+++ pages/3_AI_Parser.py
@@ -20,18 +20,34 @@ if ai_uploaded_file is not None:
     # Read JSON File
     df = pd.read_json(ai_uploaded_file)
     df["roomtype"] = np.where(df["ratedescription"].str.contains("1 sofa bed"), "Sofa Bed + ", "")
+    df["roomtype"] = np.where(df["ratedescription"].str.contains("Sofa Bed"), "Sofa Bed + ", "")
     df["roomtype"] = df["roomtype"] + np.where(df["status_code"] == 203, "Hotel not listed",
                             np.where(df["status_code"] == 202, "No rooms available",
                             np.where((df["ratedescription"].str.contains("1 queen")) & (df["ratedescription"].str.contains("1 king")), "King + Queen",
+                            np.where((df["ratedescription"].str.contains("1 Queen")) & (df["ratedescription"].str.contains("1 King")), "King + Queen",
                             np.where(df["ratedescription"].str.contains("2 queen"), "Queen Queen",
+                            np.where(df["ratedescription"].str.contains("2 Queen"), "Queen Queen",
                             np.where(df["ratedescription"].str.contains("1 queen"), "Queen",
+                            np.where(df["ratedescription"].str.contains("1 Queen"), "Queen",
                             np.where(df["ratedescription"].str.contains("2 king"), "King King",
+                            np.where(df["ratedescription"].str.contains("2 King"), "King King",
                             np.where(df["ratedescription"].str.contains("1 king"), "King",
+                            np.where(df["ratedescription"].str.contains("1 King"), "King",
                             np.where(df["ratedescription"].str.contains("2 full"), "Double Double",
+                            np.where(df["ratedescription"].str.contains("2 Full"), "Double Double",
                             np.where(df["ratedescription"].str.contains("1 full"), "Double",
+                            np.where(df["ratedescription"].str.contains("1 Full"), "Double",
+                            np.where(df["ratedescription"].str.contains("1 double"), "Double",
+                            np.where(df["ratedescription"].str.contains("1 Double"), "Double",
                             np.where(df["ratedescription"].str.contains("2 twin"), "Twin Twin",
+                            np.where(df["ratedescription"].str.contains("2 Twin"), "Twin Twin",
+                            np.where(df["ratedescription"].str.contains("2 single"), "Twin Twin",
+                            np.where(df["ratedescription"].str.contains("2 Single"), "Twin Twin",
+                            np.where(df["ratedescription"].str.contains("2 Large Single"), "Twin Twin",
                             np.where(df["ratedescription"].str.contains("1 twin"), "Twin",
+                            np.where(df["ratedescription"].str.contains("1 Twin"), "Twin",
                             np.where(df["ratedescription"].str.contains("4 bunk"), "Bunk Bunk Bunk Bunk",
+                            np.where(df["ratedescription"].str.contains("4 Bunk"), "Bunk Bunk Bunk Bunk",
                             "Other"))))))))))))
     df_export = df[["websitecode", "hotelcode", "ratedate", "roomtype", "onsiterate", "status_code", "ratedescription", "taxtype"]]
 
